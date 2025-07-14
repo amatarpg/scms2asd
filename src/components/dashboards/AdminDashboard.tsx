c68074c5bb4e1538433d69682550cee29ce578c6
amatarpg <amatarpagoyas@gmail.com>
2025-07-15 00:45:38 +0700

    Perbaiki layout dashboard agar Activity Feed di samping kanan chart

--- src/components/dashboards/AdminDashboard.tsx
+++ src/components/dashboards/AdminDashboard.tsx
@@ -240,15 +240,15 @@ const AdminDashboard: React.FC = () => {
           </div>
 
           {/* Charts Grid */}
-          {/* Main Content Grid - Bar Chart and Activity Feed */}
-          <div className="grid grid-cols-1 xl:grid-cols-10 gap-4 lg:gap-6 mb-6 lg:mb-8">
+          {/* Main Content Grid - Bar Chart and Activity Feed side by side */}
+          <div className="grid grid-cols-1 lg:grid-cols-10 gap-4 lg:gap-6 mb-6 lg:mb-8">
             {/* Bar Chart - 70% width */}
-            <div className="xl:col-span-7">
+            <div className="lg:col-span-7">
               <StudentsByMajorChart data={studentsByMajor} />
             </div>
             
             {/* Activity Feed - 30% width */}
-            <div className="xl:col-span-3">
+            <div className="lg:col-span-3">
               <ActivityFeed activities={activities} />
             </div>
           </div>
